948793a5084242554796fb94a358c99529b53f17
Lars <lstegeman94@gmail.com>
2017-10-25 17:16:15 +0200

    Switch to library

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-var cpabe = require('./cpabe/cpabe_wrapper.js');
+const cpabe = require("node-cp-abe")
 
-cpabe.setup();
-var key = cpabe.keygen('patient');
 
-var enc_data = cpabe.encrypt('hello patient', 'patient');
+// keys has public and private key.
+var keys = cpabe.setup();
 
-var dec_data = cpabe.decrypt(enc_data, key);
+//Message to decrypt.
+var message = 'hello patient 1';
 
-console.log(dec_data.toString());
+var patient1 = cpabe.keygen(keys.pubkey, keys.mstkey, ["patient = 1"]);
+var patient2 = cpabe.keygen(keys.pubkey, keys.mstkey, ["patient = 2"]);
+
+
+//encryption.
+var enc_data = cpabe.encryptMessage(keys.pubkey, 'patient=1', new Buffer(message));
+
+//decryption
+var decrypted = cpabe.decryptMessage(keys.pubkey, patient1, enc_data);
+console.log("decrypted: " + decrypted.toString());
+
+// Will not decrypt
+var decrypted = cpabe.decryptMessage(keys.pubkey, patient2, enc_data);
+console.log("decrypted: " + decrypted.toString());
+
+
+
