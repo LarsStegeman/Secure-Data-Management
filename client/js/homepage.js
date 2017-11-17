a3d4f60f180a7475d6a3d838f67dac942e8ede0e
joaoDavidGB <joaobaiao12@gmail.com>
2017-11-17 18:37:50 +0100

    navbar

--- client/js/homepage.js
+++ client/js/homepage.js
@@ -13,6 +13,34 @@ const getParams = query => {
       return params;
     }, { });
 };
+entityType = getParams(window.location.search)["type"];
+entityID = getParams(window.location.search)["id"];
 
-console.log(getParams(window.location.search)["id"]);
-console.log(getParams(window.location.search)["type"]);
+document.getElementById("navbar-user").innerHTML = entityType + " " + entityID;
+
+switch(entityType){
+  case "patient":
+    document.getElementById("nav-patient").classList.add("active");
+    break;
+  case "doctor":
+    document.getElementById("nav-doctor").classList.add("active");
+    break;
+  case "hospital":
+    document.getElementById("nav-hospital").classList.add("active");
+    break;
+  case "healthclub":
+    document.getElementById("nav-healthclub").classList.add("active");
+    break;
+  case "employer":
+    document.getElementById("nav-employer").classList.add("active");
+    break;
+  case "insurance":
+    document.getElementById("nav-insurance").classList.add("active");
+    break;
+  default:
+    console.log(entityType);
+}
+
+document.getElementById("nav-changeuser").onmousedown = function() {
+  window.location.replace("index.html");
+}
