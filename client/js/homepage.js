6e603057f538ba975265d8bc98bddf731e5e92d3
Diogo Carvalho <diogoavc@gmail.com>
2017-11-17 17:29:20 +0100

    Function to get entity parameters on client

---
+++ client/js/homepage.js
@@ -0,0 +1,18 @@
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+
+const getParams = query => {
+  if (!query) {
+    return { };
+  }
+
+  return (/^[?#]/.test(query) ? query.slice(1) : query)
+    .split('&')
+    .reduce((params, param) => {
+      let [ key, value ] = param.split('=');
+      params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+      return params;
+    }, { });
+};
+
+console.log(getParams(window.location.search)["id"]);
+console.log(getParams(window.location.search)["type"]);
